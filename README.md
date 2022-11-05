64fb5449b7a5a0dbb9fb987d918b0c98b55ba128
Juan R. Pacha <juan.ramon.pacha@upc.edu>
2022-11-05 10:37:43 +0100

    Several minor changes. In poisson_equation.m the output of the solution is now given in form of a table. linearTriangElement.m has been reformatted

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
 # 240032-Practice-2-5-Poisson-Equation-2D-linear-triangles
+You can find the session's guide ---together with other extra material files---at 
+at professor Antonio Susin's website,
+
+https://numfactory.upc.edu/blog/2018/09/03/finite-element-introduction-course/
+
+### Additional files required for the practice
+1. From <tt>additionalFiles.zip</tt>
+   * <tt>baryCoord.m</tt>
+   * <tt>plotElementsOld.m</tt>
+   * <tt>plotElements.m</tt>
+   * <tt>plotCountourSolution<tt>:w
+
+You can download <tt>additionalFiles.zip</tt> from professor Antonio Toni
+Susin's website,
+
+https://numfactory.upc.edu/numfactory/subjects/FEM/Extra/additionalFiles.zip
+
+#### Disclaimer
+This stuff is provided 'as is'. Please, check it (just in case you find 
+it useful), but it's worth you try to write all the programs on your own.
+
+If you find any mistakes (or have any suggestions), please report them to 
+
+juan.ramon.pacha@upc.edu 
+
+Many thanks,
+
+J.R.
--- poisson_equation.m
+++ poisson_equation.m
@@ -1,6 +1,27 @@
 clearvars
 close all
+% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Practice 2.5 Poisson Equation (2D Linear Triangles)
+% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+% Poisson example
+%
+% We consider the Poisson equation on a triangle domain 
+% with f(x,y) constant over the domain
 
+% Coefficients:
+% a11 = a22 = 1, a12 = a21 = a00 = 0, f = 1
+
+% The domain is meshed by linear triangles. We recall that,
+% when the coefficients of the model equafion are constant
+% over the elment, there exist "closed" formulas that give
+% the components of the local stiffness matrix. The same 
+% works for the "source" term. 
+
+% See the documents with the Theory's presentation at the
+% Numerical Factory
+
+% Geometry: nodes and elements
 nodes=[
     0,0;
     0.5,0;
@@ -9,18 +30,21 @@ nodes=[
     1,0.5;
     1,1;
     ];
+
 elem=[1,2,3;
       5,3,2;
       2,4,5;
       3,5,6;
       ];
-numNod=size(nodes,1);
-numElem=size(elem,1);
 
-numbering=1; %if you want to see number values
-plotElements(nodes,elem,numbering);
+numNod=size(nodes,1); %find out the number of nodes
+numElem=size(elem,1); %find out the nimber of elements
 
-%Coefficients 
+% Plot the meshed domain
+numbering=1; %if you want to see number of nodes and elements
+plotElementsOld(nodes,elem,numbering);
+
+% Coefficients of the modal equation 
 a11=1;
 a12=0;
 a21=a12;
@@ -34,6 +58,9 @@ Q = zeros(numNod,1);
 F = zeros(numNod,1);
 
 for e = 1:numElem
+    %
+    % Assemble the elements
+    %
     [Ke,Fe] = linearTriangElement(coeff, nodes, elem, e);
     rows = [elem(e,1), elem(e,2), elem(e,3)];
     cols = rows;
@@ -42,29 +69,39 @@ for e = 1:numElem
         F(rows) = F(rows) + Fe;
     end
 end
-
+%
 % Boundary Conditions
-fixedNodes = [4,5,6];
+%
+fixedNodes = [4,5,6]; %fixed nodes (global numbering)
 freeNodes = setdiff(1:numNod,fixedNodes);
 
-%Essential B.C.
-Q(freeNodes) = 0; %Redudant
-%Natural B.C
+% Essential B.C.
+Q(freeNodes) = 0; %In *this* case redundant, since Q
+                  %Q was initialised to zero
+% Natural B.C
 u = zeros(numNod,1);
 u(fixedNodes) = 0;
 
-%Reduced system 
+% Reduced system 
 Qm = F(freeNodes) + Q(freeNodes) - K(freeNodes,fixedNodes)*u(fixedNodes);
 Km = K(freeNodes,freeNodes);
 
+% Solution of the reduced system
 um = Km\Qm;
 u(freeNodes) = um;
 
+% Post-process
 Q = K*u - F;
 
-format short e
-[u, Q]
+% Table with solutions
+sols = [(1:numNod)',u,Q];
 
+%format short e
+%format compact
+fprintf("%5s%8s%17s\n","node","U","Q")
+fprintf("%5d%14.6e%16.6e\n",sols');
+
+% Color map for the solution 
 titol='Poisson solution';
 colorScale='jet';
 plotContourSolution(nodes,elem,u,titol,colorScale)
